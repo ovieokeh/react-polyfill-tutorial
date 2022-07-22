940fa04e6aafdb7af6afc14b715f1f656ea656cb
Ovie Okeh <ovie.okeh@sky.uk>
2022-07-22 09:39:29 +0200

    add manual padEnd polyfill

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,26 @@ function App() {
   useSimulateNoPadEnd();
 
   useEffect(() => {
-    // Your code goes here
+    if (!String.prototype.padEnd) {
+      console.log("padEnd is not supported. Using polyfill.");
+
+      String.prototype.padEnd = function padEnd(targetLength, padString) {
+        targetLength =
+          typeof targetLength !== "number" || targetLength < 0
+            ? 0
+            : targetLength;
+        padString = String(padString || " ");
+
+        if (this.length > targetLength) return String(this);
+
+        targetLength = targetLength - this.length;
+        if (targetLength > padString.length) {
+          padString += padString.repeat(targetLength / padString.length);
+        }
+
+        return String(this) + padString.slice(0, targetLength);
+      };
+    }
   }, []);
 
   return (
@@ -26,7 +45,7 @@ function App() {
 
       <div>
         {days.map((day) => {
-          const dayName = day.name; // Update this line to use padEnd
+          const dayName = day.name.padEnd(23);
 
           return (
             <div key={day.name}>
